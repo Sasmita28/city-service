f5158e487f5d40584074c5193ed56c9090d0fcdd
KotR9001 <57807780+KotR9001@users.noreply.github.com>
2020-05-27 11:40:27 -0500

    Make changes.

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,62 @@
-console.log("Hello Team")
-console.log("Hi there.")
+var department="ignore"
+var status="ignore"
+var source="ignore"
+var year = "ignore"
 
-d3.json("/query").then((data) => {
+// store variables in an array
+var variables = [department, status, source , year];
+
+// create empty arrays for manipulating elements and indexes
+var query_variables = []
+var variables_index = []
+
+// grab the elements and indexes of elements in 'variables' not equal to ignore and push into seperate arrays defined above
+variables.forEach(function(element,index) {
+    if (element != "ignore") {
+        query_variables.push(element)
+        variables_index.push(index)
+    };
+});
+
+// starting point for developing query string
+var parameter_key = ["/query?"]
+
+// logic to look at positions of elements within arrays to build query string
+variables_index.forEach(function(element, index){
+    if (element == 0 && index == 0){
+        parameter_key.push("department=")
+        parameter_key.push(query_variables[index])
+    } else if (element == 0 && index != 0) {
+        parameter_key.push("&department=")
+        parameter_key.push(query_variables[index])
+    } else if (element == 1 && index == 0) {
+        parameter_key.push("status=")
+        parameter_key.push(query_variables[index])
+    } else if (element == 1 && index != 0) {
+        parameter_key.push("&status=")
+        parameter_key.push(query_variables[index])
+    } else if (element == 2 && index == 0) {
+        parameter_key.push("source=")
+        parameter_key.push(query_variables[index])
+    } else if (element == 2 && index != 0) {
+        parameter_key.push("&source=")
+        parameter_key.push(query_variables[index])
+    } else if (element == 3 && index == 0) {
+        parameter_key.push("year=")
+        parameter_key.push(query_variables[index])
+    } else if (element == 3 && index != 0) {
+        parameter_key.push("&year=")
+        parameter_key.push(query_variables[index])
+    };
+});
+
+// string together elements of parameter_key array
+var url = parameter_key.join("");
+
+// print out url 
+console.log(url)
+
+d3.json(url).then((data) => {
     console.log("The data is here.")
     console.log(data)
 
@@ -51,4 +106,6 @@ d3.json("/query").then((data) => {
     //Assign Plot to ID
     Plotly.newPlot('average', chart_average, layout_average);
     Plotly.newPlot('count', chart_count, layout_count);
+}).catch((error) => {
+  console.error(error);
 });
--- static/js/demo.js
+++ static/js/demo.js
@@ -1,11 +1,11 @@
-const url = "http://127.0.0.1:5000";
+//const url = "http://127.0.0.1:5000";
 
 // Fetch the JSON data and console log it
 // d3.json(url).then(function(data) {
 //   console.log(data);
 // });
 
-d3.json(url, function (error, data) {
-    if(error) throw error;
-    console.log(data);
-});
+//d3.json(url, function (error, data) {
+    //if(error) throw error;
+    //console.log(data);
+//});
