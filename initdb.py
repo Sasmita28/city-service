361a170708af75e00846001eff579e90f8a2e6bf
sasmitapraveen <sasmitapraveen@gmail.com>
2020-06-01 16:02:56 -0500

    files for heroku

--- initdb.py
+++ initdb.py
@@ -1,4 +1,18 @@
 from citi_data.app import db
+import os
+import pymongo
+from flask import Flask
 
 # db.drop_all()
-db.create_all()
+MONGO_URL = os.environ.get('MONGO_URL')
+if not MONGO_URL:
+
+    MONGO_URL = "mongodb://localhost:27017"
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = MONGO_URL
+client = pymongo.MongoClient(MONGO_URL)
+
+db = client.citi_data
+calls = db.calls
